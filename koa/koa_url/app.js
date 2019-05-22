ad47bc3041ced0df640645f8550132a22eec5b13
mllet <1240752460@qq.com>
2019-05-22 17:41:48 +0800

    update

---
+++ koa/koa_url/app.js
@@ -0,0 +1,40 @@
+const Koa = require('koa');
+const app = new Koa();
+app.use(async (ctx, next) => {
+    const start = new Date().getTime();
+    await next();
+    const ms = new Date().getTime() - start;
+    console.log(`${ctx.request.method} ${ctx.request.url}: ${ms}ms`);
+    ctx.response.set('X-Response-Time', `${ms}ms`);
+});
+app.use(async (ctx, next) => {
+    await next();
+    // ctx.throw(500);
+    ctx.response.type = 'text/html';
+    ctx.response.body = '<h1>Hello, koa2!</h1>';
+});
+app.listen(9000);
+console.log('app started at port 9000...');
+//如果错误被try...catch捕获，就不会触发error事件。这时，必须调用ctx.app.emit()，手动释放error事件，才能让监听函数生效
+const handler = async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    ctx.response.status = err.statusCode || err.status || 500;
+    ctx.response.type = 'html';
+    ctx.response.body = '<p>Something wrong, please contact administrator.</p>';
+    ctx.app.emit('error', err, ctx);
+  }
+};
+
+const main = ctx => {
+  // ctx.throw(500);
+};
+
+app.on('error', function(err) {
+  console.log('logging error ', err.message);
+  console.log(err);
+});
+app.use(handler);
+app.use(main);
+app.listen(3000);
